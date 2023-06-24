4e61629d914a56c7a4c853bf8a37c1c410894c96
NormanMises <normanmises@gmail.com>
2023-06-24 21:45:35 +0800

    引体向上基本完成

--- sketch.js
+++ sketch.js
@@ -14,11 +14,8 @@ function setup() {
         // 设置画布大小为视频的宽度和高度
         // createCanvas(videoWidth, videoHeight);
         createCanvas(windowWidth, windowHeight);
+        console.log(windowWidth, windowHeight);
         video.hide();
-
-        const windowSizeText = createP();
-        windowSizeText.position(10, 10); // 设置元素的位置
-        windowSizeText.style('color', 'red'); // 设置文字颜色为白色
 
         poseNet = ml5.poseNet(video, modelLoad);
         poseNet.on('pose', gotPoses);
@@ -27,7 +24,7 @@ function setup() {
 
 
 function gotPoses(poses) {
-    console.log(poses);
+    // console.log(poses);
 
     if (poses.length > 0) {
         pose = poses[0].pose;
@@ -40,10 +37,6 @@ function modelLoad() {
 }
 
 function draw() {
-    // 更新窗口的宽度和高度
-    windowSizeText.html(`Window Size: ${windowWidth} x ${windowHeight}`);
-
-
     // 将画布的坐标系移动到画布的右上角
     // translate(video.width, 0);
     // 沿着x轴翻转画布
@@ -109,7 +102,7 @@ function drawLineBetweenHands() {
     let rightHand = pose.rightWrist;
     if (leftHand.confidence > 0.5 && rightHand.confidence > 0.5) {
         stroke(255, 0, 0);
-        strokeWeight(2);
+        strokeWeight(5);
         line(leftHand.x, leftHand.y, rightHand.x, rightHand.y);
     }
 }
