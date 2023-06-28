aa3b93bf46e62bab73b028ffa92b5a300dc1a832
codenotknock <13781678921@163.com>
2023-06-28 21:53:48 +0800

    10

---
+++ src/main/java/com/codenotknock/websocketpractice/WebsocketPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codenotknock.websocketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketPracticeApplication.class, args);
+    }
+
+}
