5e34a1dfb9bb16141479c6b12036e94d0e847b3c
NormanMises <normanmises@gmail.com>
2023-06-29 13:23:14 +0800

    单元测试

--- sketch.js
+++ sketch.js
@@ -310,6 +310,7 @@ function calculateKeypointDistance(keypoint1, keypoint2) {
 function calculateDistance(keypoint1, keypoint2) {
     return dist(keypoint1.x, keypoint1.y, keypoint2.x, keypoint2.y);
 }
+
 // 计算三个关键点所形成的角度
 function calculateAngle(pointA, pointB, pointC) {
     const AB = distanceCalculator.calculate(pointA, pointB);
@@ -364,3 +365,4 @@ function updatePullUpCounter() {
         pullUpDone = false;
     }
 }
+
